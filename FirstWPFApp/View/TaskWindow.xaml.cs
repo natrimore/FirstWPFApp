e0a10c9dde7ff66ca37dd2ab932a3847da099b7f
natrimore <jakhongir.murtazayev@gmail.com>
2020-06-04 21:45:46 +0500

    initial commit

---
+++ FirstWPFApp/View/TaskWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using FirstWPFApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstWPFApp
+{
+    /// <summary>
+    /// Interaction logic for TaskWindow.xaml
+    /// </summary>
+    public partial class TaskWindow : Window
+    {
+        public TaskWindow()
+        {
+            InitializeComponent();
+            DataContext = new TaskViewModel();
+        }
+
+        private void closeTaskWindow(object sender, RoutedEventArgs e)
+        {
+            //Owner.Visibility = Visibility.Visible;
+            this.Hide();
+        }
+    }
+}
